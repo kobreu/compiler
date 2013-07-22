ec5df4c04e80cb79221dadec70035f85c1dbcadf
Lisa Velden <lisa.velden@mytum.de>
2013-07-22 13:25:05 +0200

    junit & getting rid of generated files

---
+++ Frontend/test/unittest/TestFunctionCall.java
@@ -0,0 +1,14 @@
+package unittest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestFunctionCall {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
