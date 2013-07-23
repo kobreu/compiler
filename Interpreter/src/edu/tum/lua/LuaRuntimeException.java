3e512a04ab9422ffc292c536e99159ce99b3addb
Salomon Sickert <sickert@in.tum.de>
2013-07-23 17:01:19 +0200

    Add LuaRuntimeException.

---
+++ Interpreter/src/edu/tum/lua/LuaRuntimeException.java
@@ -0,0 +1,9 @@
+package edu.tum.lua;
+
+public final class LuaRuntimeException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public LuaRuntimeException(String message) {
+		super(message);
+	}
+}
