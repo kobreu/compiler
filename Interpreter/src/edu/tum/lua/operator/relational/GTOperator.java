917467d3eafc8f3e3e7cc83999158b8b4e011549
Johannes Mikulasch <mikulasc@in.tum.de>
2013-07-23 13:13:06 +0200

    Added "Less Than" and "Greater Than" operators

---
+++ Interpreter/src/edu/tum/lua/operator/relational/GTOperator.java
@@ -0,0 +1,17 @@
+package edu.tum.lua.operator.relational;
+
+import edu.tum.lua.operator.Operator;
+
+public class GTOperator extends Operator {
+	/* Greater Than operator: a > b 
+	 * Translate a > b to b < a
+	 */
+	
+	private LTOperator ltoperator;
+
+	public boolean apply(Object o1, Object o2) throws NoSuchMethodException {
+		
+		return ltoperator.apply(o2, o1);
+	}
+	
+}
