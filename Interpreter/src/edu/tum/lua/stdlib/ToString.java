48881a3de21364db82833c7f2f6d7fd1af4bab28
Matthias Kneidel <kneidel@in.tum.de>
2013-07-23 15:52:02 +0200

    ToString

---
+++ Interpreter/src/edu/tum/lua/stdlib/ToString.java
@@ -0,0 +1,48 @@
+package edu.tum.lua.stdlib;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import edu.tum.lua.types.LuaFunctionNative;
+import edu.tum.lua.types.LuaType;
+
+public class ToString extends LuaFunctionNative {
+
+	@Override
+	public List<Object> apply(List<Object> arguments) {
+		if (arguments.isEmpty()) {
+			// TODO error no argument
+		}
+		
+		List<Object> list = new LinkedList<Object>();
+		Object o = arguments.get(0);
+		
+		if (LuaType.getTypeOf(o) == LuaType.STRING) {
+			list.add(o);
+			return list;
+		}
+		
+		if (LuaType.getTypeOf(o) == LuaType.BOOLEAN) {
+			list.add( Boolean.toString((boolean) o) );
+			return list;
+		}
+		
+		if (LuaType.getTypeOf(o) == LuaType.NIL) {
+			list.add("nil");
+			return list;
+		}
+		
+		if (LuaType.getTypeOf(o) == LuaType.NUMBER) {
+			list.add(Double.toString((double) o));
+			return list;
+		}
+		
+		if (LuaType.getTypeOf(o) == LuaType.TABLE || LuaType.getTypeOf(o) == LuaType.FUNCTION) {
+			list.add(o.toString());
+			return list;
+		}
+		
+		return null;
+	}
+
+}
