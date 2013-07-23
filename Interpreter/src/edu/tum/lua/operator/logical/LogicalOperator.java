a77ca356e7d8defea772715b6057ce66939779a3
Johannes Mikulasch <mikulasc@in.tum.de>
2013-07-23 12:01:37 +0200

    Changed isTrue from protected to public

--- Interpreter/src/edu/tum/lua/operator/logical/LogicalOperator.java
+++ Interpreter/src/edu/tum/lua/operator/logical/LogicalOperator.java
@@ -3,7 +3,7 @@ package edu.tum.lua.operator.logical;
 import edu.tum.lua.types.LuaType;
 
 public abstract class LogicalOperator {
-	protected static boolean isTrue(Object object) {
+	public static boolean isTrue(Object object) {
 		if (LuaType.getTypeOf(object) == LuaType.NIL) {
 			return false;
 		}
