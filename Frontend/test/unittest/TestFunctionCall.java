ac6c02f1cd91c38e22f9909533712d50fedcf39d
Lisa Velden <lisa.velden@mytum.de>
2013-07-23 14:12:37 +0200

    new package structure

--- Frontend/test/unittest/TestFunctionCall.java
+++ Frontend/test/unittest/TestFunctionCall.java
@@ -2,13 +2,29 @@ package unittest;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
+import main.Main;
+
 import org.junit.Test;
+
+import visitors.Prog;
 
 public class TestFunctionCall {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		String[] args = new String[2];
+		args[0] = "testinput/arith/arith_simple.lua";
+		try {
+			Prog prog = (new Main()).run(args);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
