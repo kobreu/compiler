7d5abefb437a8aa67544085758992fb95ff0e909
Salomon Sickert <sickert@in.tum.de>
2013-07-25 14:28:24 +0200

    Fix return of print.

--- Interpreter/src/edu/tum/lua/stdlib/Print.java
+++ Interpreter/src/edu/tum/lua/stdlib/Print.java
@@ -1,36 +1,37 @@
 package edu.tum.lua.stdlib;
 
 import java.io.PrintStream;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 import edu.tum.lua.types.LuaFunctionNative;
 
 public class Print extends LuaFunctionNative {
-	
-	private ToString s = new ToString();
-	private PrintStream stdOut = System.out;
-	
+
+	private final ToString s = new ToString();
+	private final PrintStream stdOut = System.out;
+
 	@Override
-	public List<Object> apply(List<Object> arguments){
-		if (arguments.size()==0) stdOut.println();
-		else{
+	public List<Object> apply(List<Object> arguments) {
+		if (arguments.size() == 0) {
+			stdOut.println();
+		} else {
 			Object o = arguments.remove(0);
 			LinkedList<Object> arg = new LinkedList<Object>();
 			arg.add(o);
 			List<Object> l = s.apply(arg);
 			stdOut.print(l.get(0).toString());
-		for (Object v : arguments){
-			stdOut.print("      ");
-			arg = new LinkedList<Object>();
-			arg.add(v);
-			l = s.apply(arg);
-			stdOut.print(l.get(0).toString());
+			for (Object v : arguments) {
+				stdOut.print("      ");
+				arg = new LinkedList<Object>();
+				arg.add(v);
+				l = s.apply(arg);
+				stdOut.print(l.get(0).toString());
+			}
 		}
-		}
-		return null;
+
+		return Collections.emptyList();
 	}
-	
-	
 
 }
