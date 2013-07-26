1ebac431b0df2153f7e32f942d0cd2798e6ab9f2
Salomon Sickert <sickert@in.tum.de>
2013-07-26 10:51:57 +0200

    Adapt usage example.

--- Frontend/src/main/UsageExample.java
+++ Frontend/src/main/UsageExample.java
@@ -1,14 +1,14 @@
 package main;
 
-import edu.tum.lua.ast.Block;
 import util.ParserUtil;
+import edu.tum.lua.ast.Block;
 
 public class UsageExample {
 
 	public static void main(String[] args) {
 		try {
-			Block b = ParserUtil.parse("testinput/grammar/chunk_sequence_with_semicolons.lua");
-			Block b2 = ParserUtil.parse("testinput/grammar/exp_nil.lua");
+			Block b = ParserUtil.loadFile("testinput/grammar/chunk_sequence_with_semicolons.lua");
+			Block b2 = ParserUtil.loadFile("testinput/grammar/exp_nil.lua");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
