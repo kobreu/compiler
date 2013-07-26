e14cf2b0b65a91839185afc770fdf9f41f41a4e8
idelesques <isabel.delesques@gmail.com>
2013-07-26 10:56:04 +0200

    add coroutine library

---
+++ Interpreter/src/edu/tum/lua/stdlib/coroutine/Status.java
@@ -0,0 +1,31 @@
+package edu.tum.lua.stdlib.coroutine;
+
+import java.util.Arrays;
+import java.util.List;
+
+import edu.tum.lua.Preconditions;
+import edu.tum.lua.types.LuaFunctionNative;
+import edu.tum.lua.types.LuaThread;
+import edu.tum.lua.types.LuaType;
+
+public class Status extends LuaFunctionNative {
+
+	private static final LuaType[][] expectedTypes = { { LuaType.THREAD } };
+
+	@Override
+	public List<Object> apply(List<Object> arguments) {
+		Preconditions.checkArguments("coroutine.status", arguments, expectedTypes);
+		LuaThread t = (LuaThread) arguments.get(0);
+		String status = null;
+		if (t.getState() == Thread.State.RUNNABLE)
+			status = "running";
+		else if (t.isInterrupted() || t.getState() == Thread.State.NEW)
+			status = "suspended";
+		else if (t.getState() == Thread.State.TERMINATED)
+			status = "dead";
+		else
+			status = "normal";
+		return Arrays.asList((Object) status);
+	}
+
+}
