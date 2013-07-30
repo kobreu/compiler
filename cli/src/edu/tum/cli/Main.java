d1c70aa82eabadefbfcfd0537c6c1703eb988753
Matthias Kneidel <kneidel@in.tum.de>
2013-07-30 15:49:18 +0200

    basics

--- cli/src/edu/tum/cli/Main.java
+++ cli/src/edu/tum/cli/Main.java
@@ -3,15 +3,29 @@ package edu.tum.cli;
 import java.io.IOException;
 
 import jline.ConsoleReader;
+import jline.SimpleCompletor;
 
 public class Main {
 
 	public static void main(String... args) throws IOException {
+		// TODO Create Environment here
 
-		while (true) {
-			ConsoleReader reader = new ConsoleReader();
+		ConsoleReader reader = new ConsoleReader();
+		reader.setBellEnabled(false);
+		reader.setDefaultPrompt("> ");
+		reader.addCompletor(new SimpleCompletor(new String[] { "replacements" }));
+		// TODO add environment keys above
 
-			System.out.println(reader.readLine());
+		String line;
+		while ((line = reader.readLine()) != null) {
+			// keyword stack: count open blocks
+
+			System.out.println(line);
+
+			// execute Line with environment
+			// optional print non empty return lists
+
+			// update completors
 		}
 	}
 }
