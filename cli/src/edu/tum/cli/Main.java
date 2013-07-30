9bc249c4c605554ae1241c33f45c4f3f97e16181
Matthias Kneidel <kneidel@in.tum.de>
2013-07-30 15:22:46 +0200

    litte help

--- cli/src/edu/tum/cli/Main.java
+++ cli/src/edu/tum/cli/Main.java
@@ -1,8 +1,14 @@
 package edu.tum.cli;
 
+import java.io.IOException;
+
+import jline.ConsoleReader;
+
 public class Main {
 
-	public static void main(String... args) {
+	public static void main(String... args) throws IOException {
+		ConsoleReader reader = new ConsoleReader();
+
 		System.out.println("Hello");
 	}
 }
