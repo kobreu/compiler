0fdc21de2affc0843449c1cb2ffbb0172e7342d6
Matthias Kneidel <kneidel@in.tum.de>
2013-08-01 10:38:06 +0200

    help for Main.java

--- Interpreter/src/edu/tum/lua/cli/Main.java
+++ Interpreter/src/edu/tum/lua/cli/Main.java
@@ -8,17 +8,16 @@ public class Main {
 
 	public static void main(String... args) throws Exception {
 
-		CommandLine cmd = null;
+		CommandLine cmd = new CommandLine();
 
 		if (args.length > 0) {
 			switch (args[0]) {
 			case "-h":
 			case "-?":
 			case "-help":
-				System.out.println("Not implemented yet");
+				printHelp();
 				return;
 			case "-i":
-				cmd = new CommandLine();
 				if (args.length >= 2) {
 					cmd.doFile(args[1]);
 				}
@@ -37,5 +36,22 @@ public class Main {
 		}
 
 		cmd.run();
+	}
+
+	private static void printHelp() {
+		System.out
+				.println("Java based Lua Interpreter\nTUM CS CC-Labcourse 2013 K. Breu, I. Delesques-Grauby, M. Kneidel, J. Mikulasch, S. Padma, S. Sickert, L. Velden");
+		System.out.println("\nUsage: java -jar lua.jar [-options] [lua-File]\nwith following options:");
+
+		System.out.println("\t-help, -h, -?\t\tShow this help");
+		System.out
+				.println("\t-i [<File>]\t\tStarts the Interpreter in interactive mode and if a Lua-File is given as argument execute it before");
+		System.out
+				.println("\t[-f] <File>\t\tExecutes the Lua-File given as argument and exit the Interpreter afterwards");
+
+		System.out
+				.println("\nFor Help concerning Lua see the User Manual http://www.lua.org/manual/5.1/manual.html or --help in the interactive mode");
+		System.out
+				.println("\nFor more Informations see our project on https://github.com/kobreu/compiler or cantact us on cc-labcourse@t-online.de");
 	}
 }
